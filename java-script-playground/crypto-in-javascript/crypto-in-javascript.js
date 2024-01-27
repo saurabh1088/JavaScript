a4f5556907b1486bf83793135f17105c65ea7501
Saurabh Verma <saurabh1088@gmail.com>
2024-01-27 19:32:32 +0530

    JavaScript : Added sample example to explore CryptoJS encryption framework.

--- java-script-playground/crypto-in-javascript/crypto-in-javascript.js
+++ java-script-playground/crypto-in-javascript/crypto-in-javascript.js
@@ -1,3 +1,33 @@
 // =====================================================================================================================
-console.log('Hello Crypto!');
-console.log('encrypted', CryptoJS.AES.encrypt('themessage', 'thekey'));
+const encryptionKey = "thekey";
+
+let encryptedValue = CryptoJS.AES.encrypt('themessage', encryptionKey);
+console.log('encrypted', encryptedValue);
+console.log(encryptedValue.toString());
+
+let encryptButton = document.getElementById("encryptButton");
+encryptButton.addEventListener("click", performEncryption);
+
+let valueToEncryptTextInputElement = document.getElementById("encryptTextInputField");
+let encryptedValueElement = document.getElementById("encryptedValue");
+
+let decryptButton = document.getElementById("decryptButton");
+decryptButton.addEventListener("click", performDecryption);
+let decryptionDivWithShowAndHideElement = document.getElementById("decryptionDivWithShowAndHide");
+decryptionDivWithShowAndHideElement.style.display = "none";
+let decryptedValueElement = document.getElementById("decryptedValue");
+
+function performEncryption() {
+    const textToEncrypt = valueToEncryptTextInputElement.value;
+    console.log("Performing encryption for...")
+    console.log(textToEncrypt)
+    encryptedValue = CryptoJS.AES.encrypt(textToEncrypt, 'thekey');
+    encryptedValueElement.textContent = encryptedValue.toString();
+    decryptionDivWithShowAndHideElement.style.display = "block";
+}
+
+function performDecryption() {
+    console.log("Performing decryption...");
+    let decryptedValue = CryptoJS.AES.decrypt(encryptedValue, encryptionKey);
+    decryptedValueElement.textContent = decryptedValue.toString(CryptoJS.enc.Utf8);
+}
